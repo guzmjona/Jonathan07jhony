21bee88636769f7538c5f4e1ea249e163187b0a6
Jhonny G <jonathanguzman@Jonathans-MacBook-Air.local>
2024-04-14 23:46:38 -0700

    This it the second commit of the assigment :)

--- assigment_1/asciiArt.js
+++ assigment_1/asciiArt.js
@@ -1,8 +1,23 @@
 import { argv } from 'node:process';
 
 // print process.argv
+//takes in argurment from the command line
 argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
+
+  //print what the user gave you
+  console.log( 
+
+    //use the figlet package to print it in a funny font
+    //funny font is ghsot
+    figlet.textSync(`${index}: ${val}`, {
+      font: "Ghost",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    })
+  );
 });
 
 //node asciiArt.js "This is the string to turn tino ASCII art"
+
